6ae1a43a6402c493b971f83d8c56ce4ae5335a1c
Marc Cuva <mjcuva@gmail.com>
2013-06-22 13:43:30 -0500

    Added Custom Collection View Cell

---
+++ Todo Player/CollectionCell.h
@@ -0,0 +1,14 @@
+//
+//  CollectionCell.h
+//  Todo Player
+//
+//  Created by Marc Cuva on 6/21/13.
+//  Copyright (c) 2013 Marc Cuva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ListCollectionView.h"
+
+@interface CollectionCell : UICollectionViewCell
+@property (strong, nonatomic) IBOutlet ListCollectionView *lcv;
+@end
---
+++ Todo Player/CollectionCell.m
@@ -0,0 +1,14 @@
+//
+//  CollectionCell.m
+//  Todo Player
+//
+//  Created by Marc Cuva on 6/21/13.
+//  Copyright (c) 2013 Marc Cuva. All rights reserved.
+//
+
+#import "CollectionCell.h"
+
+@implementation CollectionCell
+
+
+@end
