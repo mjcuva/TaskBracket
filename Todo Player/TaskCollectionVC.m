794e70067fa49379e09bef51b6a9ce79a379d252
Marc Cuva <mjcuva@gmail.com>
2013-06-22 18:06:40 -0500

    Set Cell Size dynamically using UICollectionViewFlowLayout. Also add ListCollectionView in code

--- Todo Player/TaskCollectionVC.m
+++ Todo Player/TaskCollectionVC.m
@@ -7,15 +7,35 @@
 //
 
 #import "TaskCollectionVC.h"
+#import "CollectionCell.h"
+#import "ListCollectionView.h"
 
-@interface TaskCollectionVC () <UICollectionViewDataSource>
+@interface TaskCollectionVC () <UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
+@property (weak, nonatomic) IBOutlet UICollectionViewFlowLayout *flowLayout;
 @property (strong, nonatomic) IBOutlet UICollectionView* collectionView;
 @end
 
 @implementation TaskCollectionVC
 
+#define COLLECTION_VIEW_CELL_PADDING 20
+#define COLLECTION_VIEW_NAVBAR_OFFSET 44
+#define COLLECTION_VIEW_STATUSBAR_OFFSET 20
+#define COLLECTION_VIEW_OFFSET 10
+
 - (void)viewDidLoad{
     self.collectionView.dataSource = self;
+    [self setCollectionViewCellSize];
+}
+
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation{
+    [self setCollectionViewCellSize];
+    [self.collectionView reloadData];
+}
+
+- (void)setCollectionViewCellSize{
+    CGFloat size = self.view.frame.size.width / 2 - COLLECTION_VIEW_CELL_PADDING;
+    self.flowLayout.itemSize = CGSizeMake(size, size);
+    self.flowLayout.sectionInset = UIEdgeInsetsMake(COLLECTION_VIEW_NAVBAR_OFFSET + COLLECTION_VIEW_OFFSET + COLLECTION_VIEW_STATUSBAR_OFFSET, COLLECTION_VIEW_OFFSET, 44, COLLECTION_VIEW_OFFSET);
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
@@ -23,9 +43,17 @@
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
-    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:self.reuseID forIndexPath:indexPath];
-    id list = [self listAtIndex:indexPath.item];
-    [self updateCell:cell usingList:list];
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:[self reuseID] forIndexPath:indexPath];
+    if([cell isKindOfClass:[CollectionCell class]]){
+        CollectionCell *collectionCell = (CollectionCell *)cell;
+        if(!collectionCell.lcv){
+            ListCollectionView *lv = [[ListCollectionView alloc] initWithFrame:CGRectMake(0, 0, self.flowLayout.itemSize.width, self.flowLayout.itemSize.height)];
+            collectionCell.lcv = lv;
+            [collectionCell addSubview:lv];
+        }
+        id list = [self listAtIndex:indexPath.item];
+        [self updateCell:collectionCell usingList:list];
+    }
     return cell;
 }
 
@@ -50,3 +78,4 @@
 }
 
 @end
+    
