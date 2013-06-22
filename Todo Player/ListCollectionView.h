2dc218f67fdc8b3c6b12ca25c232cc9d6307ee2c
Marc Cuva <mjcuva@gmail.com>
2013-06-22 13:44:17 -0500

    Created custom view to go in collection view cell

---
+++ Todo Player/ListCollectionView.h
@@ -0,0 +1,13 @@
+//
+//  ListCollectionView.h
+//  Todo Player
+//
+//  Created by Marc Cuva on 6/21/13.
+//  Copyright (c) 2013 Marc Cuva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListCollectionView : UIView
+
+@end
---
+++ Todo Player/ListCollectionView.m
@@ -0,0 +1,35 @@
+//
+//  ListCollectionView.m
+//  Todo Player
+//
+//  Created by Marc Cuva on 6/21/13.
+//  Copyright (c) 2013 Marc Cuva. All rights reserved.
+//
+
+#import "ListCollectionView.h"
+
+@implementation ListCollectionView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        NSLog(@"HERE");
+        [self setBackgroundColor:[UIColor redColor]];
+    }
+    return self;
+}
+
+
+
+- (void)drawRect:(CGRect)rect
+{
+    UIBezierPath *roundedRect = [UIBezierPath bezierPathWithRoundedRect:self.bounds cornerRadius:12.0];
+    
+    [roundedRect addClip];
+    
+    [[UIColor redColor] setFill];
+    UIRectFill(self.bounds);
+}
+
+@end
