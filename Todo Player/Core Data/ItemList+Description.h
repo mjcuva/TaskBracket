b2ac42197452029207b337c2d5e9ac42feae0b29
Marc Cuva <mjcuva@gmail.com>
2013-06-26 14:41:23 -0500

    Override description in ItemList

---
+++ Todo Player/Core Data/ItemList+Description.h
@@ -0,0 +1,15 @@
+//
+//  ItemList+Description.h
+//  Todo Player
+//
+//  Created by Marc Cuva on 6/26/13.
+//  Copyright (c) 2013 Marc Cuva. All rights reserved.
+//
+
+#import "ItemList.h"
+
+@interface ItemList (Description)
+
+- (NSString *)description;
+
+@end
---
+++ Todo Player/Core Data/ItemList+Description.m
@@ -0,0 +1,17 @@
+//
+//  ItemList+Description.m
+//  Todo Player
+//
+//  Created by Marc Cuva on 6/26/13.
+//  Copyright (c) 2013 Marc Cuva. All rights reserved.
+//
+
+#import "ItemList+Description.h"
+
+@implementation ItemList (Description)
+
+- (NSString *)description{
+    return self.title;
+}
+
+@end
