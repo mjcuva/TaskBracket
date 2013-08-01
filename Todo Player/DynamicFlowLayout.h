5b324534fdb014ae32e8c3ee8828b9495bf881e2
Marc Cuva <mjcuva@gmail.com>
2013-08-01 16:48:01 -0500

    Added dynamic scrolling to TaskCollectionListVC

---
+++ Todo Player/DynamicFlowLayout.h
@@ -0,0 +1,13 @@
+//
+//  DynamicFlowLayout.h
+//  Task Bracket
+//
+//  Created by Marc Cuva on 8/1/13.
+//  Copyright (c) 2013 Marc Cuva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DynamicFlowLayout : UICollectionViewFlowLayout
+
+@end
---
+++ Todo Player/DynamicFlowLayout.m
@@ -0,0 +1,73 @@
+//
+//  DynamicFlowLayout.m
+//  Task Bracket
+//
+//  Created by Marc Cuva on 8/1/13.
+//  Copyright (c) 2013 Marc Cuva. All rights reserved.
+//
+
+#import "DynamicFlowLayout.h"
+
+@implementation DynamicFlowLayout{
+    UIDynamicAnimator *_dynamicAnimator;
+}
+
+
+- (void)prepareLayout{
+    [super prepareLayout];
+    
+    if(!_dynamicAnimator){
+        _dynamicAnimator = [[UIDynamicAnimator alloc] initWithCollectionViewLayout:self];
+        
+        CGSize contentSize = [self collectionViewContentSize];
+        NSArray *items = [super layoutAttributesForElementsInRect:CGRectMake(0, 0, contentSize.width, contentSize.height)];
+        
+        for(UICollectionViewLayoutAttributes *item in items){
+            UIAttachmentBehavior *spring = [[UIAttachmentBehavior alloc] initWithItem:item attachedToAnchor:item.center];
+            
+            spring.length = 0;
+            spring.damping = .5;
+            spring.frequency = .8;
+            
+            [_dynamicAnimator addBehavior:spring];
+        }
+    }
+}
+
+- (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect{
+    return [_dynamicAnimator itemsInRect:rect];
+}
+
+- (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath{
+    return [_dynamicAnimator layoutAttributesForCellAtIndexPath:indexPath];
+}
+
+- (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds{
+    UIScrollView *scrollView = self.collectionView;
+    CGFloat delta = newBounds.origin.y - scrollView.bounds.origin.y;
+    CGPoint touchLocation = [scrollView.panGestureRecognizer locationInView:scrollView];
+    
+    for(UIAttachmentBehavior *spring in _dynamicAnimator.behaviors){
+        CGPoint anchorPoint = spring.anchorPoint;
+        CGFloat distanceFromTouch = fabsf(anchorPoint.y - touchLocation.y);
+        CGFloat scrollResistance = distanceFromTouch / 500;
+        
+        UICollectionViewLayoutAttributes *attr = [spring.items firstObject];
+        CGPoint center = attr.center;
+        
+        if(delta > 0){
+            center.y += MIN(delta * scrollResistance, delta);
+        }else{
+            center.y += MAX(delta *scrollResistance, delta);
+        }
+        
+        attr.center = center;
+        
+        [_dynamicAnimator updateItemUsingCurrentState:attr];
+    }
+    
+    
+    return NO;
+}
+
+@end
