8095214ac44c4e7c39d16ee36ca82a578f7db61b
Marc <mjcuva@gmail.com>
2013-08-24 21:58:30 -0500

    Added blurring to UIImage from Apple’s WWDC code

---
+++ Todo Player/UIImage+blur.h
@@ -0,0 +1,17 @@
+//
+//  UIImage+blur.h
+//  Task Bracket
+//
+//  Created by Marc Cuva on 8/24/13.
+//  Copyright (c) 2013 Marc Cuva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (blur)
+
+- (UIImage *)applyLightEffect;
+
+- (UIImage *)applyDarkEffect;
+
+@end
